d39ac473d9497f80c0d9441cf1937661838049b3
Kinetic Poem <git@gitlab.com:flexdev/app-modules.git>
2024-10-22 13:53:47 +0300

    initial configuration

---
+++ server-app/resources/js/components/app.vue
@@ -0,0 +1,11 @@
+<template>
+    <nav>
+        <div style="padding: 10px; display: flex; justify-content: space-around;">
+            <router-link class="link" to="/">Home</router-link>
+            <router-link class="link" to="/about">About</router-link>
+        </div>
+    </nav>
+    <main>
+        <router-view></router-view>
+    </main>
+</template>
